deeb126f5c783ff68c50f4db1db3bda878c67413
komadreja70 <komadreja70@yahoo.es>
2018-05-08 19:15:56 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -100,7 +100,7 @@ function cargarNoticias() {
 }
 
 function cargarFichero(nombreFichero) {
-    var path = "https://raw.githubusercontent.com/komadreja70/noticies/master/data/" + nombreFichero;
+    var path = "https://rawgit.com/komadreja70/noticies/master/data/" + nombreFichero;
 
     //carrega noticia
     $.getJSON(path, function (jsonObject) {
